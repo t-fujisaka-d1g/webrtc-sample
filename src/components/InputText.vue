295e58fe9b55754c07b3c4411fa49319a373d26e
t-fujisaka <t-fujisaka@daiichi-g.co.jp>
2022-01-25 11:04:34 +0900

    ViewLogin画面にAPIキーを入力するフォームを実装

---
+++ src/components/InputText.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="input-text">
+    <v-text-field
+      v-model="localValue"
+      v-bind:readonly="readonly"
+      v-bind:disabled="disabled"
+      outlined
+      dense
+      v-bind:label="label"
+      hide-details
+      v-bind:prepend-inner-icon="icon"
+      v-bind:placeholder="placeholder"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, SetupContext, computed } from '@vue/composition-api'
+
+type Value = string | null
+type Props = {
+  value: string
+  label: string | null
+  readonly: boolean
+  disabled: boolean
+  icon: string | null
+  placeholder: string | null
+}
+export default defineComponent({
+  components: {},
+  props: {
+    value: { type: String, default: null },
+    label: { type: String, default: null },
+    readonly: { type: Boolean, default: false },
+    disabled: { type: Boolean, default: false },
+    icon: { type: String, default: null },
+    placeholder: { type: String, default: null },
+  },
+  emits: ['input'],
+  setup(props: Props, context: SetupContext) {
+    const emitInput = (value: Value) => {
+      context.emit('input', value)
+    }
+    const localValue = computed<Value>({
+      get: () => props.value,
+      set: (value: Value) => {
+        if (value === null || value.length === 0) {
+          emitInput(null)
+        } else {
+          emitInput(value)
+        }
+      },
+    })
+    return {
+      localValue,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.input-text {
+  padding-top: 8px;
+  padding-bottom: 8px;
+  width: 100%;
+}
+</style>
--- src/views/ViewLogin.vue
+++ src/views/ViewLogin.vue
@@ -1,20 +1,48 @@
 <template>
-  <div>ViewLogin</div>
+  <div>
+    <v-card max-width="480px" style="margin: auto" outlined>
+      <v-card-text>
+        <InputText v-model="apiKey" label="APIキー" />
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn text color="primary" v-bind:disabled="disabled" v-on:click="clickDone">
+          <v-icon left>mdi-check</v-icon>
+          設定
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from '@vue/composition-api'
+import { computed, defineComponent, reactive, toRefs } from '@vue/composition-api'
+import InputText from '@/components/InputText.vue'
+import { RouterHelper } from '@/router/helper'
 
-type State = {}
-type Props = {}
+type State = {
+  apiKey: string
+}
 export default defineComponent({
-  components: {},
+  components: { InputText },
   setup() {
-    // state
-    const state = reactive<State>({})
+    const state = reactive<State>({
+      apiKey: '',
+    })
 
+    const disabled = computed<boolean>(() => state.apiKey.length === 0)
+
+    const clickDone = async () => {
+      const apiKey = state.apiKey
+      if (apiKey.length === 0) {
+        return null
+      }
+      await RouterHelper.moveHome(apiKey)
+    }
     return {
       ...toRefs(state),
+      disabled,
+      clickDone,
     }
   },
 })
